f5b8cfeba409c609394d2a0325d30be6e73d1db5
Snezhana Petlova <snpetlova@gmail.com>
2023-11-08 17:12:29 +0200

    User-List-Workshop
    format date

--- User-List-Workshop/client/src/components/UserListItem.jsx
+++ User-List-Workshop/client/src/components/UserListItem.jsx
@@ -1,3 +1,5 @@
+import { formatDate } from "../utils/dataUtils";
+
 const UserListItem = ({
     firstName,
     lastName,
@@ -19,7 +21,7 @@ const UserListItem = ({
       <td>{lastName}</td>
       <td>{email}</td>
       <td>{phoneNumber}</td>
-      <td>{createdAt}</td>
+      <td>{formatDate(createdAt)}</td>
 
       <td className="actions">
         <button className="btn edit-btn" title="Edit">
