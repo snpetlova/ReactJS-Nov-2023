015405ff8179ebbb2e66d1db391a0def83e436dd
Snezhana Petlova <snpetlova@gmail.com>
2023-11-08 17:00:33 +0200

    User-List-Workshop
    Project configuration, user list item, user list table and user service

--- User-List-Workshop/client/src/components/UserList.jsx
+++ User-List-Workshop/client/src/components/UserList.jsx
@@ -3,10 +3,13 @@ import UserListTable from "./UserListTable";
 
 const UserList = () => {
   return (
-  <section className="card users-container">
-    <Search />
-    <UserListTable />
-  </section>
+    <section className="card users-container">
+      <Search />
+      <UserListTable />
+
+      <button className="btn-add btn">Add new user</button>
+      
+    </section>
   );
 };
 
