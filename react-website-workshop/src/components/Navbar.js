5b54ddb0448fb08ea823b6953d7bb8ac340c04ae
Snezhana Petlova <snpetlova@gmail.com>
2023-11-10 14:11:36 +0200

    react website workshop
    building a simple website using react

---
+++ react-website-workshop/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='leftSide'></div>
+        <div className='rightSide'></div>
+    </div>
+  )
+}
+
+export default Navbar
